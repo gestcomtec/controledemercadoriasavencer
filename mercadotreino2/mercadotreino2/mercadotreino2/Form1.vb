7aacd6180ca8addb1c92e97a5680ffc4ef5a688e
gestcomtec <gestcomtecmicrogestao@gmail.com>
2025-10-18 20:58:45 -0300

    Add files via upload

---
+++ mercadotreino2/mercadotreino2/mercadotreino2/Form1.vb
@@ -0,0 +1,20 @@
+﻿' Form1 - Tela de Login
+Public Class Form1
+    ' Evento do botão de login
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        ' Defina aqui seu nome de usuário e senha para validação
+        Dim username As String = txtUsername.Text
+        Dim password As String = txtPassword.Text
+
+        ' Validação simples de login (substitua isso por um banco de dados ou outra lógica mais segura)
+        If username = "alves" And password = "mercado" Then
+            ' Se o login for válido, abre o Form2 (Dashboard)
+            Dim dashboardForm As New Form2
+            dashboardForm.Show()   ' Abre o Form2
+            Me.Hide()  ' Esconde o Form1 (Login)
+        Else
+            ' Se as credenciais estiverem erradas, exibe uma mensagem de erro
+            MessageBox.Show("Credenciais inválidas! Tente novamente.")
+        End If
+    End Sub
+End Class
---
+++ mercadotreino2/mercadotreino2/mercadotreino2/Form2.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvProdutos = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.btnAdicionar = New System.Windows.Forms.Button()
+        Me.btnRemover = New System.Windows.Forms.Button()
+        Me.btnAtualizar = New System.Windows.Forms.Button()
+        Me.btnSair = New System.Windows.Forms.Button()
+        CType(Me.dgvProdutos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvProdutos
+        '
+        Me.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvProdutos.Location = New System.Drawing.Point(77, 127)
+        Me.dgvProdutos.Name = "dgvProdutos"
+        Me.dgvProdutos.Size = New System.Drawing.Size(478, 218)
+        Me.dgvProdutos.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Dashboard mercadinho alves"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(74, 505)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(60, 13)
+        Me.lblTotal.TabIndex = 2
+        Me.lblTotal.Text = "quantidade"
+        '
+        'btnAdicionar
+        '
+        Me.btnAdicionar.Location = New System.Drawing.Point(77, 375)
+        Me.btnAdicionar.Name = "btnAdicionar"
+        Me.btnAdicionar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdicionar.TabIndex = 3
+        Me.btnAdicionar.Text = "adcionar"
+        Me.btnAdicionar.UseVisualStyleBackColor = True
+        '
+        'btnRemover
+        '
+        Me.btnRemover.Location = New System.Drawing.Point(158, 375)
+        Me.btnRemover.Name = "btnRemover"
+        Me.btnRemover.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemover.TabIndex = 4
+        Me.btnRemover.Text = "remover"
+        Me.btnRemover.UseVisualStyleBackColor = True
+        '
+        'btnAtualizar
+        '
+        Me.btnAtualizar.Location = New System.Drawing.Point(239, 375)
+        Me.btnAtualizar.Name = "btnAtualizar"
+        Me.btnAtualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAtualizar.TabIndex = 5
+        Me.btnAtualizar.Text = "atualizar"
+        Me.btnAtualizar.UseVisualStyleBackColor = True
+        '
+        'btnSair
+        '
+        Me.btnSair.Location = New System.Drawing.Point(320, 375)
+        Me.btnSair.Name = "btnSair"
+        Me.btnSair.Size = New System.Drawing.Size(75, 23)
+        Me.btnSair.TabIndex = 6
+        Me.btnSair.Text = "Sair"
+        Me.btnSair.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1409, 751)
+        Me.Controls.Add(Me.btnSair)
+        Me.Controls.Add(Me.btnAtualizar)
+        Me.Controls.Add(Me.btnRemover)
+        Me.Controls.Add(Me.btnAdicionar)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvProdutos)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.dgvProdutos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvProdutos As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents btnAdicionar As Button
+    Friend WithEvents btnRemover As Button
+    Friend WithEvents btnAtualizar As Button
+    Friend WithEvents btnSair As Button
+End Class
